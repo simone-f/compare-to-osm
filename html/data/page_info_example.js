7eab9daec9cfe45770f6123dffb6fbf573f1dda0
Simone F <groppo8@gmail.com>
2015-07-27 20:39:17 +0200

    Set map's view with parameters from page_info.js

--- html/data/page_info_example.js
+++ html/data/page_info_example.js
@@ -1,7 +1,7 @@
 var title = 'Confronto fra strade in OpenStreetMap ed open data';
-var mapLat = 45.44;
-var mapLon = 10.99;
-var mapZoom = 12;
+var mapLat = 41.8921;
+var mapLon = 12.4832;
+var mapZoom = 6;
 var info = '<b>Strade da mappare in OpenStreetMap</b>';
 info += '<p>Confronto fra strade in <a href="http://www.openstreetmap.org/" target="_blank">OSM</a> e i dati rilasciati da:';
 info += '<br>- <a href="http://www.comune.verona.it/nqcontent.cfm?a_id=37426" target="_blank">Comune di Verona</a>  <a href="http://www.dati.gov.it/iodl/2.0/" target="_blank">IODL 2.0</a>';
@@ -11,4 +11,5 @@ info += '<br><br>Dati OSM: ';
 for (i in tasks) {
     info += '<br><i>- ' + tasks[i].name + ' ' + tasks[i]["analysis_time"] + '</i>';
 }
+info += '<br><br>Clicca sulla mappa per modificare in JOSM.';
 info += '<br><br>Pagina creata tramite: <a href="https://github.com/simone-f/compare-to-osm" target="_blank">compare-to-osm</a>.';
